67e9c3ceb7be2b6acedc29285cc73034a0fe1d48
bcmastercard@gmail.com <bcmastercard@gmail.com>
2025-09-20 15:12:12 +0000

    fct sales created

--- dim_tables/dim_currency_conversion.sql
+++ dim_tables/dim_currency_conversion.sql
@@ -1,5 +1,5 @@
 WITH dim_currency_conversion AS (
-  SELECT 'Africa' AS region, CAST(0.945 AS NUMERIC) AS currency
+  SELECT 'Africa' AS region, CAST(0.945 AS NUMERIC) AS currency_conversion
   UNION ALL SELECT 'Asia', CAST(0.987 AS NUMERIC)
   UNION ALL SELECT 'Europe', CAST(1.151 AS NUMERIC)
   UNION ALL SELECT 'North America', CAST(1.000 AS NUMERIC)
